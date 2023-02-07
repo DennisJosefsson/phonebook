f377dc3ce544cb8cdb59710cf1a1484d31fa84c9
Dennis Josefsson <dennis.josefsson@gmail.com>
2023-02-07 11:05:30 +0100

    Second commit

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   {
     id: 1,
@@ -24,12 +26,67 @@ let persons = [
   },
 ]
 
+const generateId = () => {
+  const id = Math.floor(Math.random() * 100000)
+  return id
+}
+
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
+app.get('/info', (request, response) => {
+  const time = new Date()
+  const string = `<p>Phonebook has info for ${persons.length} people</p> 
+  <p>${time}</p>`
+  response.send(string)
+})
+
 app.get('/api/persons', (request, response) => {
   response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find((p) => p.id === id)
+  if (person) {
+    response.json(person)
+  }
+  response.status(404).end()
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter((p) => p.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'Name or number is missing',
+    })
+  }
+
+  if (persons.some((person) => person.name === body.name)) {
+    return response.status(400).json({
+      error: `The name ${body.name} already exists in the phonebook`,
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    date: new Date(),
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
 })
 
 const PORT = 3001
